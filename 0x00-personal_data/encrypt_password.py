8c1d79cee20f2f01abcbca61caccb45ccdf1a582
Gloman21 <irankundab21ertin@gmail.com>
2024-08-01 13:07:32 +0200

    task 2

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+ Hashes a password using the bcrypt algorithm.
+ Check if a given password matches a hashed password.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using the bcrypt algorithm
+    """
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode("utf-8"), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checking if a given password matches a hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
